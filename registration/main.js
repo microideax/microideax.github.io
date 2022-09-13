557281bb638d9191fcd1fdd232fb8c5a74726072
Yao Chen <yaochen_nk@hotmail.com>
2022-09-13 22:43:31 +0800

    cacm-eao-2022-1

---
+++ registration/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+
+    function footerPosition() {
+        $("footer").removeClass(".fixed-bottom");
+        var contentHeight = document.body.scrollHeight,
+        
+             winHeight = window.innerHeight; 
+        if (! (contentHeight > winHeight)) {
+            $("footer").addClass("fixed-bottom");
+        } else {
+            $("footer").removeClass("fixed-bottom");
+        }
+
+    }
+
+    footerPosition();
+
+    $(window).resize(footerPosition);
+
+});
